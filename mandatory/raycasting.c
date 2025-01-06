b431a6d5065fcc2c1195b74908e6439a6c508713
banditklm <kaltoum.elmounjid123@gmail.com>
2025-01-06 21:56:00 +0100

    raycasting...

--- mandatory/raycasting.c
+++ mandatory/raycasting.c
@@ -2,12 +2,25 @@
 
 void    raycast(t_data *data)
 {
-    double  angle;
+    double  ray_angle;
     double  segment;
     double  start_angle;
+    double  raydir_x;
+    double  raydir_y;
     int     i;
+    int     screen_width;
 
     segment = data->player.fov / data->map_width;
-    start_angle = 
-    while (i)
+    start_angle = atan2(data->player.y_dir, data->player.x_dir) - (data->player.fov / 2)
+    screen_width = data->screen_width;
+    while (i < screen_width)
+    {
+        ray_angle = start_angle + i * segment;
+        raydir_x = cos(ray_angle);
+        raydir_y = sin(ray_angle);
+        trace_ray();
+        i++;
+    }
 }
+
+
